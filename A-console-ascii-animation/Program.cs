60c5917013f98ef23c7f58d8959815d86e0eee23
bsimjoo <bsimjoo@gmail.com>
2020-02-12 18:14:15 +0330

    Add project files.

---
+++ A-console-ascii-animation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_console_ascii_animation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
